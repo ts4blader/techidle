5548d915ade6fcb8540d8d1761f674c5f705f617
ts4blader <ts4blader@gmail.com>
2021-11-16 22:10:46 +0700

    add link to button on click

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Icon from "./Icon";
+import { useRouter } from "next/router";
 
-export default function Button({ text, img, type }) {
+export default function Button({ text, img, type, link }) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (!link) return;
+    router.push(link);
+  };
+
   return (
-    <div className="button" data-type={type}>
+    <div className="button" data-type={type} onClick={handleClick}>
       {text && <p className="text">{text}</p>}
       {img && <Icon src={img} />}
     </div>
