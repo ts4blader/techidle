0985598960f20170bdc5c18ebf12e94a98759f48
ts4blader <ts4blader@gmail.com>
2021-11-28 22:15:45 +0700

    style login form

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SigninForm from "../components/SigninForm";
+import Image from "../components/Image";
 
 export default function LoginPage() {
   return (
     <div className="login-page">
       <SigninForm />
+      <Image src="svg/login.svg" alt="login" />
     </div>
   );
 }
